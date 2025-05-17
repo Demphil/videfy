3617863296786457c77af0b0de73e5f44bee46ac
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-17 03:50:32 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',  // مهم جداً لو المشروع يُنشر في مجلد فرعي أو بدون دومين فرعي
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
 });
+
