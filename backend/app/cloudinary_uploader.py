572bb4360d4f888bef2995081a3f300a8565a096
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-20 02:08:10 +0100

    Update cloudinary_uploader.py

--- backend/app/cloudinary_uploader.py
+++ backend/app/cloudinary_uploader.py
@@ -1,5 +1,6 @@
 import cloudinary
 import cloudinary.uploader
+import os
 
 # إعداد معلومات الحساب
 cloudinary.config(
@@ -8,10 +9,7 @@ cloudinary.config(
     api_secret="Bfmj-xzKmruB8NDT5cfIhRaHZg8"
 )
 
-def upload_image_to_cloudinary(image_path):
-    try:
-        response = cloudinary.uploader.upload(image_path)
-        return response["secure_url"]
-    except Exception as e:
-        print("❌ خطأ في رفع الصورة:", e)
-        return None
+
+def upload_image(file_path):
+    response = cloudinary.uploader.upload(file_path)
+    return response["secure_url"]
