aecf7dce84b98bacc1a9bd14b6eed43a5dd37853
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-20 04:23:02 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,8 +8,8 @@ from app.cloudinary_uploader import upload_media_to_cloudinary
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="backend/static"), name="static")
+templates = Jinja2Templates(directory="backend/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
